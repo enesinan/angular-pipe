1ef67b31f064f794345053880f6e5203162dfb12
enes.inan <enes.inan@vbt.com.tr>
2022-12-13 17:03:37 +0300

    pipe section add

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+  pure: false // like re-render, performance issues
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filteredString: string, propName: string): any {
+    if(value.length === 0 || filteredString == ''){
+      return value;
+    }
+    const resultArray = [];
+
+    for(const item of value){
+      if(item[propName] === filteredString){
+         resultArray.push(item)
+      }
+
+    }
+    return resultArray
+  }
+
+}
